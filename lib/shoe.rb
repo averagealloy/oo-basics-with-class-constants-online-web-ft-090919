0914a7e9fec541e4b34e070fab66ba41dd0a04d1
Mike McTighe <m.mctighe10@gmail.com>
2019-09-17 19:29:13 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -8,9 +8,13 @@ class Shoe
     @brand = brand
   end
 
+  def brand=(brand)
+    BRANDS << brand
+  end
+
   def cobble
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
-  
+
 end
